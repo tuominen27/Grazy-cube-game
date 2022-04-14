090beacd51f735cec6671c654f5eaa374c1cfab4
Juhani Katajamäki <juhani.m.katajamaki@utu.fi>
2022-04-14 10:29:17 +0000

    Update .gitlab-ci.yml, testit/test_main.py, src/main.py Deleted testit/dummy

---
+++ testit/test_main.py
@@ -0,0 +1,11 @@
+import unittest
+import main
+
+class Testit(unittest.TestCase):
+
+    def testi1(self):
+        self.assertEqual(putki, None)
+
+
+if __name__ == '__main__':
+    unittest.main()
