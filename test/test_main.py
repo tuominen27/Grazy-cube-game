c78c88aebd5d8bc8b2bff170ae14a74770f3474d
Juhani Katajamäki <juhani.m.katajamaki@utu.fi>
2022-04-14 20:50:09 +0000

    Update test/test_main.py

--- test/test_main.py
+++ test/test_main.py
@@ -1,8 +1,9 @@
 import unittest
+import os
 from src.main import putki
 #import pygame
 
 class Testit(unittest.TestCase):
 
-    def testi1(self):
+    def testi_putki(self):
         self.assertEqual(putki, None)
