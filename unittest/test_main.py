5c18dcfc6ee9b695ae6ac2436322db2226fd3327
Juhani Katajamäki <juhani.m.katajamaki@utu.fi>
2022-04-15 09:58:24 +0000

    Update src/Projekti.py, src/main.py, unittest/test_main.py

--- unittest/test_main.py
+++ unittest/test_main.py
@@ -1,10 +1,10 @@
 import unittest
-from src.main import putki
+from src.main import main
 
 class Testit(unittest.TestCase):
 
-    def testi_putki(self):
-        self.assertEqual(putki) is None
+    def testi_main(self):
+        self.assertEqual(main) is None
 
 if __name__ == '__main__':
     unittest.main()
