e5faa5153883906408a64a523a621d92558dcf54
Juhani Katajamäki <juhani.m.katajamaki@utu.fi>
2022-04-15 09:31:55 +0000

    Update unittest/test_main.py, .gitlab-ci.yml

--- unittest/test_main.py
+++ unittest/test_main.py
@@ -1,11 +1,7 @@
 import unittest
-import os
 from src.main import putki
 
 class Testit(unittest.TestCase):
 
     def testi_putki(self):
         self.assertEqual(putki) is None
-
-#if __name__ == '__main__':
-#    unittest.main()
