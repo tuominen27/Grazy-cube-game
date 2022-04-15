bbd029d2e09aea9a8d2674a8707dfd28031d11ef
Juhani Katajamäki <juhani.m.katajamaki@utu.fi>
2022-04-15 08:52:40 +0000

    Update test/test_main.py, .gitlab-ci.yml

--- test/test_main.py
+++ test/test_main.py
@@ -1,9 +1,8 @@
 import unittest
 import os
 from src.main import putki
-#import pygame
 
 class Testit(unittest.TestCase):
 
     def testi_putki(self):
-        self.assertEqual(putki, None)
+        self.assertEqual(putki) is None
