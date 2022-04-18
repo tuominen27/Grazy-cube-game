599b07470fbb2646a84a9a6478ab84cd3e1fcb46
Juhani Katajamäki <juhani.m.katajamaki@utu.fi>
2022-04-18 13:49:19 +0000

    Update src/Peli.py, test_main.py Deleted .gitlab-ci.yml, src/__init__.py, unittest/__init__.py, main.py

--- test_main.py
+++ test_main.py
@@ -1,8 +1,8 @@
 import unittest
-from main import main
+from Peli import main
 
 class Testit(unittest.TestCase):
 
     def testi_main(self):
-        self.assertEqual(main) is None
+        self.assertEqual() is None
 
